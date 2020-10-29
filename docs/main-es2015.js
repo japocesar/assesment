efde0180b6617deb1bec65351f0b329428811aa5
japocesar <cesar.carrasco.s@hotmail.com>
2020-10-29 10:13:48 -0600

    build

--- docs/main-es2015.js
+++ docs/main-es2015.js
@@ -270,7 +270,8 @@ AppModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
 
 // AoT requires an exported function for factories
 function HttpLoaderFactory(http) {
-    return new _ngx_translate_http_loader__WEBPACK_IMPORTED_MODULE_16__["TranslateHttpLoader"](http);
+    // return new TranslateHttpLoader(http);
+    return new _ngx_translate_http_loader__WEBPACK_IMPORTED_MODULE_16__["TranslateHttpLoader"](http, './assets/i18n/', '.json');
 }
 
 
--- docs/main-es5.js
+++ docs/main-es5.js
@@ -277,7 +277,8 @@ var AppModule = /** @class */ (function () {
 
 // AoT requires an exported function for factories
 function HttpLoaderFactory(http) {
-    return new _ngx_translate_http_loader__WEBPACK_IMPORTED_MODULE_16__["TranslateHttpLoader"](http);
+    // return new TranslateHttpLoader(http);
+    return new _ngx_translate_http_loader__WEBPACK_IMPORTED_MODULE_16__["TranslateHttpLoader"](http, './assets/i18n/', '.json');
 }
 
 
